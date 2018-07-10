8406cb5c51bded4e7306c9de4bbd77e733830e31
Zart Seri <zartseri@yahoo.com>
2018-07-10 16:08:28 -0400

    Checkpoint 5 library: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,7 +1,11 @@
-import React from 'react';
-const Album = () => (
-  <section className="album">
-  Album will go here
-  </section>
-);
+import React, { Component } from 'react';
+class Album extends Component {
+  render() {
+    return (
+      <section className="album">
+      {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 export default Album;
