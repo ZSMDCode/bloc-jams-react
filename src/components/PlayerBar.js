520aa2987033f0cef5e0bb91c5e1a6bda838b8db
Zart Seri <zartseri@yahoo.com>
2018-07-19 21:41:16 -0400

    Assignment 8 revision completed

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -7,7 +7,7 @@ class PlayerBar extends Component {
       <button id="previous" onClick={this.props.handlePrevClick}>
       <span className="ion-skip-backward"></span>
       </button>
-      <button id="play-pause" onClick={this.props.handleSongClick} >
+      <button id="play-pause" onClick={this.props.handleSongClick}>
       <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
       </button>
       <button id="next" onClick={this.props.handleNextClick}>
@@ -16,12 +16,12 @@ class PlayerBar extends Component {
       </section>
       <section id="time-control">
       <div className="current-time">–:––</div>
-      <input type="range" className="seek-bar" value="0" />
+      <input type="range" className="seek-bar" />
       <div className="total-time">–:––</div>
       </section>
       <section id="volume-control">
       <div className="icon ion-volume-low"></div>
-      <input type="range" className="seek-bar" value="80" />
+      <input type="range" className="seek-bar" />
       <div className="icon ion-volume-high"></div>
       </section>
       </section>
